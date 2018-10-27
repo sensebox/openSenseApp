8e014a359d996621c8846588bbb20f4c9faf5ee2
Saphir El-Kaiy <saphir.elkaiy@gmail.com>
2018-10-27 14:57:04 +0200

    add getSenseBoxes, add getClosestSenseBoxes

--- src/pages/sense-box/sense-box.ts
+++ src/pages/sense-box/sense-box.ts
@@ -1,4 +1,4 @@
-import { ApiProvider } from './../../providers/api/api';
+import { ApiProvider, Location } from './../../providers/api/api';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
@@ -17,6 +17,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SenseBoxPage {
 
   boxData: any;
+  allBoxes: any;
+  closesz
+  closestBoxes: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private api: ApiProvider) {
   }
@@ -33,4 +36,36 @@ export class SenseBoxPage {
 
   }
 
+  getSenseBoxes(){
+    this.api.getSenseBoxes().subscribe(res => {
+      console.log(res);
+      this.allBoxes = res;
+    })
+  }
+
+  getClosestSenseBox(){
+    let myLocation : Location = {
+      longitude : 7.6261347, 
+      latitude : 51.9606649
+    }
+    let closestSenseBoxes = [];
+    this.api.getAllSenseBoxes().subscribe(res => {
+      let allBoxes : any = res;
+      allBoxes.forEach(element => {
+        let boxLocation : Location = {
+          latitude : element.currentLocation.coordinates[1], 
+          longitude : element.currentLocation.coordinates[0]
+        };
+        let distance : number = this.api.calculateDistance(myLocation, boxLocation);
+        if (distance <= 50) {
+          closestSenseBoxes.push(element);
+        }
+      });
+       
+      this.closestBoxes = closestSenseBoxes;
+      console.log(this.closestBoxes);
+     
+    });
+
+  }
 }
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Location {
+  latitude : number;
+  longitude : number;
+}
+
 /*
   Generated class for the ApiProvider provider.
 
@@ -20,5 +25,24 @@ export class ApiProvider {
   getData(){
     return this.http.get(`${this.API_URL}/boxes/5b0d436fd40a290019ef444d`);
   }
+  
+  getSenseBoxes(){
+    return this.http.get(`${this.API_URL}/boxes?exposure=outdoor,mobile,unknown`); 
+  }
 
+  getAllSenseBoxes(): any {
+    return this.http.get(`${this.API_URL}/boxes?exposure=outdoor,unknown`);
+  }
+
+  public calculateDistance(userLocation : Location, senseBoxLocation : Location) {
+    // Deg --> Rad
+    let lat1 = userLocation.latitude*Math.PI/180;
+    let lat2 = senseBoxLocation.latitude*Math.PI/180;
+    let lng1 = userLocation.longitude*Math.PI/180;
+    let lng2 = senseBoxLocation.longitude*Math.PI/180;
+    // distance calculation:
+    let cosG = Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1);
+    let dist = 6378.388 * Math.acos(cosG);
+    return dist;
+};
 }

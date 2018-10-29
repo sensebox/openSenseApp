a83de8cb1375362e7265fcb95290acedc9895488
Ubuntu <n_phil01@uni-muenster.de>
2018-10-29 11:51:17 +0100

    Load all senseBoxes near to 20km to the map; TODO: change design of markers, specify the nearest

--- src/pages/sensify-map/sensify-map.ts
+++ src/pages/sensify-map/sensify-map.ts
@@ -4,7 +4,8 @@ import { LeafletModule } from "@asymmetrik/ngx-leaflet";
 import { latLng, tileLayer} from "leaflet";
 import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from "@ionic-native/geolocation";
 import * as L from "leaflet";
-
+import { ApiProvider } from '../../providers/api/api';
+import {Location} from "../../providers/model";
 
 @IonicPage()
 @Component({
@@ -18,8 +19,11 @@ export class SensifyMapPage {
   public posMarker;
   public userLng: number;
   public userLat: number;
+  public closestBoxes: any;
+  public closestBoxesMarkers: any[] = [];
+  public senseBoxesSet: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public LM: LeafletModule, private geolocation: Geolocation) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public LM: LeafletModule, private geolocation: Geolocation, private api: ApiProvider) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
@@ -33,7 +37,6 @@ export class SensifyMapPage {
         this.currentPos = pos;
         this.userLat = this.currentPos.coords.latitude;
         this.userLng = this.currentPos.coords.longitude;
-        console.log(pos);
         this.loadPositionOnMap();
       }, (err: PositionError) => {
         console.log("ERROR: " + err.message)
@@ -59,12 +62,12 @@ export class SensifyMapPage {
     if(this.posMarker != undefined){
       this.map.removeLayer(this.posMarker);
     }
-    console.log(this.posMarker);
     this.map.panTo(new L.LatLng(this.userLat, this.userLng));
     this.posMarker = L.marker([this.userLat, this.userLng]).on('click', () => {
       alert('user: lat:'+ this.userLat +", lon:"+ this.userLng);
     });
     this.posMarker.addTo(this.map);
+    this.findNearestSenseBoxes();
   }
 
   // Watch the user position
@@ -73,6 +76,46 @@ export class SensifyMapPage {
       this.userLat = position.coords.latitude;
       this.userLng = position.coords.longitude;
       this.loadPositionOnMap();
-      console.log(position.coords.longitude + ' ' + position.coords.latitude);
+      this.findNearestSenseBoxes();
     });
+
+  // Search the nearest senseBoxes
+  findNearestSenseBoxes(){
+    let myLocation : Location = {
+      longitude : this.userLng,
+      latitude : this.userLat
+    };
+
+    this.api.getClosestSenseBoxes(myLocation).subscribe(res => {
+      this.closestBoxes = res;
+      if(this.senseBoxesSet){
+        this.deleteNearestSenseboxes();
+      } else {
+        this.addNearestSenseboxes();
+      }
+    });
+  }
+
+  // Delete existing senseBoxes
+  deleteNearestSenseboxes(){
+    for(let i = 0; i < this.closestBoxesMarkers.length; i++){
+      this.map.removeLayer(this.closestBoxesMarkers[i]);
+      if(i >= this.closestBoxesMarkers.length - 1){
+        this.closestBoxesMarkers = [];
+        this.addNearestSenseboxes();
+      }
+    }
+  }
+
+  // Add senseBoxes to Map
+  addNearestSenseboxes(){
+    this.senseBoxesSet = true;
+    for(let i = 0; i < this.closestBoxes.length; i++){
+      let marker = L.marker([this.closestBoxes[i].coordinates.latitude, this.closestBoxes[i].coordinates.longitude]).on('click', () => {
+        alert('senseBox: lat:'+ this.userLat +", lon:"+ this.userLng);
+      });
+      this.closestBoxesMarkers.push(marker);
+      this.closestBoxesMarkers[i].addTo(this.map);
+    }
+  }
 }

d392a73362e9f0500f049297909da6e5db4be2d3
Ubuntu <n_phil01@uni-muenster.de>
2018-10-29 09:41:59 +0100

    Live update of user-position

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -21,6 +21,9 @@ export class MapPage {
 
   options : GeolocationOptions;
   currentPos : Geoposition;
+  public posMarker;
+  public userLng: number;
+  public userLat: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public LM: LeafletModule, private geolocation: Geolocation) {
   }
@@ -35,6 +38,8 @@ export class MapPage {
     this.geolocation.getCurrentPosition(this.options)
       .then((pos: Geoposition) => {
         this.currentPos = pos;
+        this.userLat = this.currentPos.coords.latitude;
+        this.userLng = this.currentPos.coords.longitude;
         console.log(pos);
         this.loadPositionOnMap();
       }, (err: PositionError) => {
@@ -42,6 +47,7 @@ export class MapPage {
       })
   }
 
+  // load the map, load the layer
   map: L.Map;
   // Leafet Map options
   LeafletOptions = {
@@ -55,15 +61,25 @@ export class MapPage {
     this.map = map;
   }
 
+  // Center the map on the user-position
   loadPositionOnMap(){
-    console.log("Should change map position");
-    this.map.panTo(new L.LatLng(this.currentPos.coords.latitude, this.currentPos.coords.longitude));
-    let markerGroup = L.featureGroup();
-    let marker = L.marker([this.currentPos.coords.latitude, this.currentPos.coords.longitude]).on('click', () => {
-      alert('user: lat:'+ this.currentPos.coords.latitude +", lon:"+ this.currentPos.coords.longitude);
+    if(this.posMarker != undefined){
+      this.map.removeLayer(this.posMarker);
+    }
+    console.log(this.posMarker);
+    this.map.panTo(new L.LatLng(this.userLat, this.userLng));
+    this.posMarker = L.marker([this.userLat, this.userLng]).on('click', () => {
+      alert('user: lat:'+ this.userLat +", lon:"+ this.userLng);
     });
-    markerGroup.addLayer(marker);
-    this.map.addLayer(markerGroup);
+    this.posMarker.addTo(this.map);
   }
 
+  // Watch the user position
+  subscription = this.geolocation.watchPosition()
+    .subscribe(position => {
+      this.userLat = position.coords.latitude;
+      this.userLng = position.coords.longitude;
+      this.loadPositionOnMap();
+      console.log(position.coords.longitude + ' ' + position.coords.latitude);
+    });
 }

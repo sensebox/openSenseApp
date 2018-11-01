1510822448a911de2210c200c489af536cef8d7b
Viktor Gorte <v_gort01@uni-muenster.de>
2018-11-01 17:37:16 +0100

    Added setting inputs to sensify-about
    - switching between tabs looses the inputs
    - Work on the TODOs

--- src/app/sensify-components/sensify-about/sensify-about.ts
+++ src/app/sensify-components/sensify-about/sensify-about.ts
@@ -6,12 +6,23 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'sensify-about.html',
 })
 export class SensifyAboutPage {
+  newRadius : any;
+  newValidationRange : any;
+  newSenseboxID : any
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SensifyAboutPage');
+  }
+
+  //TODO
+  //-save inputs in other variables on button press so that they dont vanish when switching between tabs
+  //-show current settings in the fields
+  //-Think about other setting options that we need
+  test(){
+    alert("radius:"+this.newRadius+",       validationRange:"+this.newValidationRange+",        SenseBoxID:"+this.newSenseboxID)
   }
 
 }

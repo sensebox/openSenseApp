9e727d2845a8171771252f39071e8d0ce13e5cb3
Albert <albert@WL-POOL2-PNT-11248.UNI-MUENSTER.DE>
2018-11-08 17:31:48 +0100

    closed #19
    - added radar rain map
    - added pull to refresh function --> deleted refresh button

--- src/pages/weather-app/weather-app.ts
+++ src/pages/weather-app/weather-app.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ApiProvider} from "../../providers/api/api";
 
-/**
- * Generated class for the WeatherAppPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -25,18 +19,33 @@ export class WeatherAppPage {
     this.refresh_data();
   }
 
-  refresh_data(){
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
     this.api.getData().subscribe(res => {
       console.log(res);
       this.boxData = res;
-    console.log('Refresh was clicked');
-    })
+      console.log('Refresh was clicked');
+    });
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
   }
 
-  /* auto update?
-  task = setInterval(() => {
-    this.refresh_data();
-  }, 1000000);
+refresh_data(){
+  this.api.getData().subscribe(res => {
+    console.log(res);
+    this.boxData = res;
+  console.log('Refresh was clicked');
+  })
+}
+
+/*
+auto update?
+task = setInterval(() => {
+  this.refresh_data();
+}, 1000000);
 */
 
   locate_click(){

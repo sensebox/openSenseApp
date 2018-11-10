3dc7b032f05306f01a6e3939852b209cbeef79ce
Viktor Gorte <v_gort01@uni-muenster.de>
2018-11-10 15:06:38 +0100

    Added new structure for starting page + cleanup

--- src/app/sensify-components/sensify-start/sensify-start.ts
+++ src/app/sensify-components/sensify-start/sensify-start.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
-import { ApiProvider } from '../../../providers/api/api';
-import { Geolocation } from "@ionic-native/geolocation";
-import { NativeGeocoder } from '@ionic-native/native-geocoder';
 import { SenseBox, Metadata } from '../../../providers/model';
 import { SensifyPage } from '../../../pages/sensify/sensify-page';
 
@@ -15,10 +12,11 @@ export class SensifyStartPage implements OnChanges {
 
     @Input()
     public metadata: Metadata;
-
     public currBox: SenseBox;
+    public date: String;
 
-    constructor(public mySensifyPage:SensifyPage,public platform: Platform, public navCtrl: NavController, public navParams: NavParams, private api: ApiProvider, private geolocation: Geolocation, private nativeGeocoder: NativeGeocoder) {
+    constructor(public mySensifyPage:SensifyPage,public platform: Platform, public navCtrl: NavController, public navParams: NavParams) {
+        this.setCurrentDate();
         // TODO: take mySenseBox from metadata.settings and not from API request again
         this.mySensifyPage.getMetadata();
         // this.currBox = this.metadata.settings.mySenseBox ? this.metadata.settings.mySenseBox : this.api.getclosestSenseBoxTest(boxes, pos.coords);
@@ -28,6 +26,15 @@ export class SensifyStartPage implements OnChanges {
         console.log('ionViewDidLoad SensifyStartPage');
     }
     
+    setCurrentDate(){
+        var currentDate = new Date()
+        var day = currentDate.getDate()
+        var month = currentDate.getMonth() + 1 //January is 0!
+        var year = currentDate.getFullYear()
+        console.log(day + "." + month + "." + year);
+        this.date = "Today: "+day + "." + month + "." + year;
+    }
+
     ngOnChanges(changes) {
         if (changes.metadata.currentValue.closestSenseBox) {
             this.currBox = this.metadata.closestSenseBox;
--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { SenseBox } from '../model';
 import * as L from "leaflet";

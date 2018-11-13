45691b960b59c7cf92717cba6f40f449b9f8d9b4
SarahLechler <lechler.sarah@gmail.com>
2018-11-13 09:03:16 +0100

    boxLayer in popover

--- src/pages/leaflet/leaflet.ts
+++ src/pages/leaflet/leaflet.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild, ElementRef} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import leaflet from 'leaflet';
+import $ from "jquery";
 
 /**
  * Generated class for the LeafletPage page.
@@ -17,6 +18,8 @@ import leaflet from 'leaflet';
 export class LeafletPage {
   @ViewChild('map') mapContainer: ElementRef;
   map: any;
+  boxData: any;
+  boxLayer: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -26,7 +29,7 @@ export class LeafletPage {
     console.log('ionViewDidLoad LeafletPage');
   }
 
-  loadMap(){
+  loadMap() {
     this.map = leaflet.map('map').setView([51.505, -0.09], 13);
 
     leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -36,9 +39,58 @@ export class LeafletPage {
     leaflet.marker([51.5, -0.09]).addTo(this.map)
       .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
       .openPopup();
+
+    this.loadSenseboxLayer();
   }
 
-    
-  
+  loadSenseboxLayer() {
+    let featureArray = [];
+    $.getJSON('https://api.opensensemap.org/boxes?exposure=outdoor', data => {
+      let jsonData = JSON.stringify(data);
+      jsonData = JSON.parse(jsonData);
+      console.log(jsonData);
+      this.boxData = jsonData;
+      this.boxLayer = leaflet.geoJSON('', {
+        id: 'jsonLayer',
 
+      }).addTo(this.map);
+
+
+      this.boxData.forEach((entry) => {
+        let geojsonFeature = _createGeojsonFeaturen(entry);
+        this.boxLayer.addData(geojsonFeature)
+      });
+      this.boxData.forEach(box => {
+        featureArray.push(box);
+      });
+    });
+
+  }
 }
+
+let
+  getJsonData = () => {
+    $.getJSON('https://api.opensensemap.org/boxes?', data => {
+      let jsonData = JSON.stringify(data);
+      jsonData = JSON.parse(jsonData);
+      return jsonData;
+    })
+  };
+let
+  _createGeojsonFeaturen = (entry) => {
+    let geojsonFeature = {
+      "type": "Feature",
+      "properties": {
+        "name": entry.Name,
+        "amenity": entry.Name,
+        "entry": entry,
+        "popupContent": 'GoodBox!'
+
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [parseFloat(entry.currentLocation.coordinates[0]), parseFloat(entry.currentLocation.coordinates[1])]
+      }
+    };
+    return geojsonFeature;
+  };

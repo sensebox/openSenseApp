3eef7a34cc590e9583ef0b0d44e1fcb301900061
rkans02 <rkans02@users.noreply.github.com>
2018-11-13 02:19:55 +0100

    Sunrise and Sunset
    Added Sunrise and Sunset request from "https://sunrise-sunset.org/api" and added the information to the startview

--- src/app/sensify-components/sensify-start/sensify-start.ts
+++ src/app/sensify-components/sensify-start/sensify-start.ts
@@ -14,12 +14,58 @@ export class SensifyStartPage implements OnChanges {
     public metadata: Metadata;
     public currBox: SenseBox;
     public date: String;
+    public sunrise: String;
+    public sunset: String;
 
     constructor(public mySensifyPage:SensifyPage,public platform: Platform, public navCtrl: NavController, public navParams: NavParams) {
         this.setCurrentDate();
         // TODO: take mySenseBox from metadata.settings and not from API request again
-        this.mySensifyPage.getMetadata();
+        this.init();
+
+        
+
+
         // this.currBox = this.metadata.settings.mySenseBox ? this.metadata.settings.mySenseBox : this.api.getclosestSenseBoxTest(boxes, pos.coords);
+        
+        
+        
+    
+    }
+
+    public async init() {
+        try {
+            await this.mySensifyPage.getMetadata().then(meta => {
+                this.metadata = meta;
+                this.currBox= this.metadata.closestSenseBox;
+            });
+            await this.getSunrise("https://api.sunrise-sunset.org/json?lat=" + this.metadata.settings.location.lat + "&lng=" + this.metadata.settings.location.lng + "&date=today").then(sun => {
+                this.sunrise = JSON.parse(sun).results.sunrise;
+                this.sunset = JSON.parse(sun).results.sunset;
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    
+getSunrise(url: string): Promise<any> {
+    return new Promise<any>(
+      function (resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.onload = function () {
+          if (this.status === 200) {
+            resolve(this.response);
+          } else {
+            reject(new Error(this.statusText));
+          }
+        };
+        request.onerror = function () {
+          reject(new Error('XMLHttpRequest Error: ' + this.statusText));
+        };
+        request.open('GET', url);
+        request.send();
+      });
     }
 
     ionViewDidLoad() {

828068e0f6606e496bdbc2dbae93e1ff04bfc04f
Albert <albert@WL-POOL4-PNT-01507.UNI-MUENSTER.DE>
2018-11-29 11:20:38 +0100

    changed senseBox ID
    new senseBox is selected to get right measurements

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
 export class ApiProvider {
 
   private API_URL = 'https://api.opensensemap.org/boxes/';
-  private boxId ='5a687677411a790019444133';
+  private boxId ='5bb610bf043f3f001b6a4c53';
   private sat24Location = 'DE'; // https://en.sat24.com/en/freeimages
   private sat24Url ='https://api.sat24.com/animated/'+this.sat24Location+'/rainTMC/2/Central%20European%20Standard%20Time/9553668';
 

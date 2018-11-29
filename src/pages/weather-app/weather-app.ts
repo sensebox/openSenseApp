06ac30f4424f89e39ed764fc1254745094ab2f85
Albert <albert@Alfs-MacBook.local>
2018-11-29 10:15:55 +0100

    new style & map

--- src/pages/weather-app/weather-app.ts
+++ src/pages/weather-app/weather-app.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import {ApiProvider} from '../../providers/api/api';
-
 import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
 import { LeafletPage } from '../leaflet/leaflet';
+//import leaflet from "../leaflet/leaflet";
+import leaflet from 'leaflet';
 
 
 @IonicPage()
@@ -13,6 +13,8 @@ import { LeafletPage } from '../leaflet/leaflet';
   })
 
 export class WeatherAppPage {
+  @ViewChild('rainviewerMap')mapContainer: ElementRef;
+  rainviewerMap:any;
   boxData: any;
   constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController, private api: ApiProvider) {
   }
@@ -20,8 +22,16 @@ export class WeatherAppPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad WeatherAppPage');
     this.refresh_data();
+    this.loadRainViewerMap();
   }
 
+  loadRainViewerMap() {
+    this.rainviewerMap = leaflet.map('rainviewerMap').fitWorld();
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'openSense WeatherApp',
+      maxZoom: 18
+    }).addTo(this.rainviewerMap)
+  }
 
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);

1dc4dea8371eeaab641ac1ab7437abb02559cbb9
Albert <albert@WL-POOL4-PNT-02242.UNI-MUENSTER.DE>
2018-12-10 10:49:23 +0100

    delete old radar URL

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -12,8 +12,7 @@ export class ApiProvider {
 
   private API_URL = 'https://api.opensensemap.org/boxes/';
   private boxId ='5a687677411a790019444133';
-  private sat24Location = 'DE'; // https://en.sat24.com/en/freeimages
-  private sat24Url ='https://api.sat24.com/animated/'+this.sat24Location+'/rainTMC/2/Central%20European%20Standard%20Time/9553668';
+
 
   constructor(public http: HttpClient) {
     console.log('Hello ApiProvider Provider');

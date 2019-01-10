48531d0b22ed2f905f639943f55aab9b9521a6d6
BKaric <b_kari02@uni-muenster.de>
2019-01-10 01:13:39 +0100

    integrate mqtt over websockets, enable subscription of mqtt data, integrate live data into highchart, visualize data dynamically

--- src/pages/sense-box/sense-box.ts
+++ src/pages/sense-box/sense-box.ts
@@ -1,7 +1,7 @@
-import { ApiProvider } from './../../providers/api/api';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Paho } from 'ng2-mqtt/mqttws31';
+import * as HighCharts from 'highcharts';
 /**
  * Generated class for the SenseBoxPage page.
  *
@@ -17,20 +17,103 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class SenseBoxPage {
 
   boxData: any;
+  client: any;
+  message: any;
+  datArray: number[] = []; 
+  chart: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private api: ApiProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+
+      this.chart = HighCharts.chart('xValues', {
+        chart: {
+          type: 'spline'
+        },
+        title: {
+          text: 'Acceleration on X-Axis'
+        },
+        xAxis: {
+          title: {
+            text: 'Measurements'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Value of Acceleration'
+          }
+        },
+        series: [{
+          data: this.datArray
+          }]
+      });
+    
+
+    // Create a client instance
+    this.client = new Paho.MQTT.Client('localhost', 11883, "clienId");
+    
+
+    // set callback handlers
+    this.client.onConnectionLost = this.onConnectionLost;
+    this.client.onMessageArrived = this.onMessageArrived;
+
+    // connect the this.client
+    this.client.connect({onSuccess:this.onConnect.bind(this)});
+    console.log(this.client);
     console.log('ionViewDidLoad SenseBoxPage');
   }
 
-  getData(){
-    this.api.getData().subscribe(res => {
-      console.log(res);
-      this.boxData = res;
-    })
-
+   // called when the this.client connects
+  onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    console.log(this.client);
+    //this.client.subscribe("$SYS/#");
+    this.client.subscribe("/World");
+    this.message = new Paho.MQTT.Message('1');
+    this.message.destinationName = "/World";
+    this.client.send(this.message); 
   }
 
-}
+  // called when the this.client loses its connection
+  onConnectionLost(responseObject) {
+    console.log(responseObject);
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  // called when a message arrives
+  onMessageArrived = (message) => {
+    console.log("onMessageArrived:", message.destinationName, message.payloadString);
+    this.datArray.push(+message.payloadString);
+    console.log(this.datArray);
+    this.chart.series[0].setData(this.datArray);
+    // this.reloadHighchart();
+  }
+
+//   reloadHighchart() {
+//     chart = HighCharts.chart('xValues', {
+//       chart: {
+//         type: 'spline'
+//       },
+//       title: {
+//         text: 'Acceleration on X-Axis'
+//       },
+//       xAxis: {
+//         title: {
+//           text: 'Measurements'
+//         }
+//       },
+//       yAxis: {
+//         title: {
+//           text: 'Value of Acceleration'
+//         }
+//       },
+//       series: [{
+//         data: this.datArray
+//         }]
+//     });
+//   }
+ }
